cc13f520460993a786b53ac52dc973d7ef4d74bf
Blasphemouz <raul.adamson@gmail.com>
2023-02-02 06:02:22 +0200

    Uus tavakasutaja roll nimega "User" ning eraldi piiratud paneel User rolli jaoks

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-require 'kirby/bootstrap.php';
+require __DIR__ . '/kirby/bootstrap.php';
 
-echo (new Kirby)->render();
+$kirby = new Kirby();
+
+$currentUser = $kirby->user();
+
+if ( $currentUser && $currentUser->role()->name() === 'admin' ):
+    $kirby = new Kirby([
+        'roots' => [
+            'blueprints' => __DIR__ . '/site/blueprints',
+        ],
+    ]);
+
+elseif ( $currentUser && $currentUser->role()->name() === 'user' ):
+    $kirby = new Kirby([
+        'roots' => [
+            'blueprints' => __DIR__ . '/site/blueprints-user',
+        ],
+    ]);
+endif;
+
+echo $kirby->render();
