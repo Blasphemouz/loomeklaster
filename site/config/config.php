13e3bfb1dd66b4521240211ee1f5dee29c4c683d
Blasphemouz <raul.adamson@gmail.com>
2023-02-14 17:20:55 +0200

    Peale postituse muutmist, selle staatus muutub automaatselt mustandiks edaspidiseks kinnitamiseks.

--- site/config/config.php
+++ site/config/config.php
@@ -5,6 +5,20 @@ return [
         'install' => true,
         'css' => 'assets/css/custom-panel.css'
     ],
+    'hooks' => [
+        'page.*:after' => function ($event, $newPage) { //Otsib muudatusi lehtedele
+            if(in_array($event->action(), ['update', 'changeTitle', 'changeSlug',]) !== true) { //Kood ei reageeri sündmustele, mis ei sobi
+                return;
+            }
+
+            $role = $this->user()->role(); //Leiab kastuaja rolli
+
+            if($role == 'user') { //Kui kastuaja roll on tavakasutaja
+                $newPage->changeStatus('draft'); //Muudab postituse staatuse draftiks
+                return $newPage;
+            }
+        }
+    ],
     'languages' => true,
 	'debug'  => false,
 ];
